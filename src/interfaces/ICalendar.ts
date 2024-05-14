d6d6234ed9a18a70540e66efef0225f8be529664
Sergey Akkuratov <sergey.akkuratov@gmail.com>
2024-05-14 18:35:57 +0300

    Добавлены тесты на фильтры по умолчанию

--- src/interfaces/ICalendar.ts
+++ src/interfaces/ICalendar.ts
@@ -2,7 +2,12 @@ import { IFilter, IFilters } from "./IFilter";
 import { ITask, TaskStatus } from "./ITask";
 
 export const defaultFilters: IFilters<ITask> = {
-  byDate: (element: ITask, date: Date) => new Date(element.date) === date,
+  byDate: (element: ITask, date: Date | string) => {
+    if (date instanceof Date) {
+      return new Date(element.date).getTime() === date.getTime();
+    }
+    return element.date === date;
+  },
   byDateBetween: (element: ITask, start: Date, end: Date) => {
     const elementDate = new Date(element.date);
     return elementDate >= start && elementDate <= end
@@ -10,7 +15,7 @@ export const defaultFilters: IFilters<ITask> = {
   byName: (element: ITask, name: string) => element.name === name,
   byNameContains: (element: ITask, name: string) => element.name.includes(name),
   byStatus: (element: ITask, status: TaskStatus) => element.status === status,
-  byTag: (element: ITask, tags: string[]) => element.tags === tags,
+  byTag: (element: ITask, tags: string[]) => JSON.stringify(element.tags) === JSON.stringify(tags),
   byTagContains: (element: ITask, tag: string) => element.tags.includes(tag),
   byDescription: (element: ITask, text: string) => element.description === text,
   byDescriptionContains: (element: ITask, text: string) =>
